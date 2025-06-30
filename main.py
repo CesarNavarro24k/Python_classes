8234a4bea37a552132cf0232569ee848c20014ad
Larrix14k <larrix@gmail.com>
2025-06-30 13:25:13 -0500

    tercer commit

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 class Persona:
-    def __init__(self, name, age, hobbie, password):
+    def __init__(self, name, age, hobbie, password, user):
         self.name = name
         self.age = age
         self.hobbie = hobbie
         self.password = password
+        self.user = user
 
-        if len(password) < 3:
+        if len(password) <= 3:
             print("Tu contraseña no debe tener menos de 3 caracteres")
-        elif len(password) > 12:
+        elif len(password) > 3 and len(password) <= 12:
             print("Tu contraseña no debe tener más de 12 caracteres")
         else:
             print(f"""
 El nombre de la persona es: {self.name} y tiene {self.age} y le gusta {self.hobbie}
-Su contraseña es {self.password}
+Su contraseña es {self.password} y su nombre de usuario es: {user}
 """)
 
 
@@ -20,6 +21,7 @@ name = input("Ingresa un nombre: ")
 age = input("Ingresa una edad: ")
 hobbie = input("Ingresa algo que te guste: ")
 password = input("Ingresa una contraseña: ")
+user = input("Ingresa tu nombre de usuario:")
 
-print("Hola desde rama2")
-Persona(name, age, hobbie, password)
+
+Persona(name, age, hobbie, password, user)
